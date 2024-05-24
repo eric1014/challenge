558b65cf478e7cc62faf1a6205c0f25533d1d0c6
eric1014 <ericrodrigues.cloud@gmail.com>
2024-05-23 23:13:10 -0300

    Upando java

--- 2SPRINT/domain_driver_designing/challengeJava/challenge2Java/src/br/com/fiap/autorecomenda/views/Main.java
+++ 2SPRINT/domain_driver_designing/challengeJava/challenge2Java/src/br/com/fiap/autorecomenda/views/Main.java
@@ -3,32 +3,75 @@ package br.com.fiap.autorecomenda.views;
 import br.com.fiap.autorecomenda.models.*;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Criação de objetos
+        Scanner scanner = new Scanner(System.in);
+
+        // Criação do catálogo de peças
         CatalogoPecas catalogoPecas = new CatalogoPecas();
         catalogoPecas.adicionarPeca("Filtro de Óleo", 50.0);
         catalogoPecas.adicionarPeca("Pneu", 300.0);
         catalogoPecas.adicionarPeca("Velas de Ignição", 40.0);
+        catalogoPecas.adicionarPeca("Óleo do Motor", 150.0);
 
         CalculadoraOrcamento calculadora = new CalculadoraOrcamento(catalogoPecas);
 
-        Cliente cliente = new Cliente("João Silva", "11987654321", new Veiculo("Toyota", "Corolla", "ABC-1234", null));
+        // Interação com o usuário
+        System.out.println("Bem-vindo ao AutoRecomenda!");
 
-        Mecanica mecanica = new Mecanica("Oficina do Zé", "Rua das Flores, 123");
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
 
-        OrdemDeServico ordem = new OrdemDeServico("Troca de óleo e filtro", cliente, mecanica);
-        ordem.adicionarPeca(new Peca("Filtro de Óleo", 50.0));
-        ordem.adicionarPeca(new Peca("Óleo do Motor", 150.0));
+        System.out.print("Digite o telefone do cliente: ");
+        String telefoneCliente = scanner.nextLine();
+
+        System.out.print("Digite a marca do veículo: ");
+        String marcaVeiculo = scanner.nextLine();
+
+        System.out.print("Digite o modelo do veículo: ");
+        String modeloVeiculo = scanner.nextLine();
+
+        System.out.print("Digite a placa do veículo: ");
+        String placaVeiculo = scanner.nextLine();
+
+        Veiculo veiculo = new Veiculo(marcaVeiculo, modeloVeiculo, placaVeiculo, null);
+        Cliente cliente = new Cliente(nomeCliente, telefoneCliente, veiculo);
+        veiculo.setCliente(cliente);
+
+        System.out.print("Digite a descrição do problema: ");
+        String descricaoProblema = scanner.nextLine();
+
+        System.out.print("Digite as peças necessárias (separadas por vírgula): ");
+        String pecasNecessariasInput = scanner.nextLine();
+        List<String> pecasNecessarias = Arrays.asList(pecasNecessariasInput.split(",\\s*"));
+
+        OrdemDeServico ordem = new OrdemDeServico(descricaoProblema, cliente, null);
+        for (String peca : pecasNecessarias) {
+            double preco = catalogoPecas.buscarPrecoPeca(peca);
+            ordem.adicionarPeca(new Peca(peca, preco));
+        }
 
         cliente.adicionarOrdemDeServico(ordem);
+
+        System.out.print("Digite o nome da oficina: ");
+        String nomeOficina = scanner.nextLine();
+
+        System.out.print("Digite o endereço da oficina: ");
+        String enderecoOficina = scanner.nextLine();
+
+        Mecanica mecanica = new Mecanica(nomeOficina, enderecoOficina);
+        ordem.setMecanica(mecanica);
+        mecanica.adicionarOrdemDeServico(ordem);
 
         CentralRedirecionamento central = new CentralRedirecionamento();
         central.adicionarOficina(mecanica);
 
         // Execução de funcionalidades
-        System.out.println("Orçamento para troca de peças: " + calculadora.calcularOrcamento(Arrays.asList("Filtro de Óleo", "Pneu")));
+        double orcamento = calculadora.calcularOrcamento(pecasNecessarias);
+        System.out.println("Orçamento para troca de peças: " + orcamento);
 
         System.out.println("Cliente: " + cliente.getNome());
         System.out.println("Telefone: " + cliente.getTelefone());
@@ -41,5 +84,7 @@ public class Main {
         for (Mecanica oficina : central.buscarOficinas()) {
             System.out.println("- " + oficina.getNome() + ", " + oficina.getEndereco());
         }
+
+        scanner.close();
     }
 }
